5b543e962d4229afa32557c9b1b8b4baee572b44
Nick Olson <olsonnick10@yahoo.com>
2020-03-11 23:22:39 -0700

    Added session checking to all routes, updated session checking to set a "mode" in the context

--- middleware/check_user_session.js
+++ middleware/check_user_session.js
@@ -1,16 +1,36 @@
-module.exports = function(req, res, next) {
+module.exports = function(indexContext) {
 
-    if (req.session.user && req.cookies.user_sid) {
+    return function(req, res, next) {
 
-        // if the user is logged in already this session
-        if (req.originalUrl == '/login' || req.originalUrl == '/signup') { res.redirect('/index'); }
-        else { next(); }
+        // if user is logged in this session
+        if (req.session.user && req.cookies.user_sid) {
+            // Set logged-in status in index context
+            indexContext.headerDropdownMode = "logged-in";
 
-    } else {
+            // Catch unnecessary requests
+            if (req.originalUrl == '/login' || req.originalUrl == '/signup') { res.redirect('/index'); }
 
-        if (req.originalUrl == '/login' || req.originalUrl == '/signup') { next(); }
-        else { res.redirect('/login'); }
-        // next();
-    }
+            // Move on
+            else { next(); }
+
+        }
+
+        // if user is not logged in this session
+        else {
+            // Set logged-in status in index context
+            indexContext.headerDropdownMode = "logged-out";
+
+            // Prevent unnecessary redirect loop
+            if (req.originalUrl == '/login' || req.originalUrl == '/signup') { next(); }
+
+            // If destination doesn't care about login
+            else if (req.originalUrl == '/404') { next(); }
+
+            // Redirect to login page
+            else { res.redirect('/login'); }
+
+        }
+
+    };
 
 };
